65be1706805b983f93bb9d7eb2bb566aa5b8097e
Seungpyo Han <olok95@naver.com>
2021-01-13 16:27:38 +0900

    Add: 라이브러리추가 및 메인화면 레이아웃

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-2021 세미나 발표 프로젝트
+# 2021 세미나 발표 프로젝트
+
+## Commit Convention
+[Action] : [작업내용]  
+Action: Add/Delete/Edit  
+작업내용: 작업내용  
+ex) Add: 레이아웃 추가  
--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './App.css';
+import "./App.css";
+import { Layout } from "antd";
+import styled from 'styled-components'
+const { Header, Content } = Layout;
 
+const LayoutContainer = styled(Layout)`
+ height: 100vh;
+ display: flex;
+ flex-direction: column;
+`
+const ContentContainer = styled(Content)`
+ flex: 1;
+`
 function App() {
   return (
-    <div className="App">
-     <h1>Trollo</h1>
-    </div>
+      <LayoutContainer>
+        <Header>Header</Header>
+        <ContentContainer>Content</ContentContainer>
+      </LayoutContainer>
+
   );
 }
 
