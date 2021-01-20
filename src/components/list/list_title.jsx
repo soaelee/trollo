06f6b2bd666621515cb9ec4ae628328a6176f37e
pyo <olok95@naver.com>
2021-01-20 13:53:18 +0900

    merge

--- src/components/list/list_title.jsx
+++ src/components/list/list_title.jsx
@@ -7,61 +7,53 @@ import { useDispatch, useSelector } from 'react-redux';
 import { removeListRequestAction } from '../../reducers/board';
 import { likeListRequestAction, unlikeListRequestAction } from '../../reducers/user';
 
+
 const Container = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 0;
-  position: relative;
+display: flex;
+justify-content: space-between;
+align-items: center;
+padding: 0;
+position: relative;
 `
 const PopOverPos = styled.div`
-    position: absolute;
-    right: 0;
-    border: 0;
-    background: transparent;
+  position: absolute;
+  right: 0;
+  border: 0;
+  background: transparent;
 `;
-
 const ListTitle = ({title, id, likes}) => {
-  const dispatch = useDispatch();
-<<<<<<< HEAD
-=======
-
-  const likes = useSelector(state => state.user)
-  const likedBoards = likes.user.like
-  console.log(likedBoards)
->>>>>>> a32d54f51c903ad97304afd93d9249075f9cfaa1
-  const onClickLikeBtn = () => {
-    if(likes.includes(id)){
-      dispatch(unlikeListRequestAction(id))
-    } else {
-      dispatch(likeListRequestAction(id))
+const dispatch = useDispatch();
+const onClickLikeBtn = () => {
+  if(likes.includes(id)){
+    dispatch(unlikeListRequestAction(id))
+  } else {
+    dispatch(likeListRequestAction(id))
+  }
+};
+const onClickRemoveList = () => {
+  dispatch(removeListRequestAction(id));
+};
+return (
+  <Container>
+    <Title title={title} type='list' id={id}/>
+    <PopOverPos>
+    { likes && likes.includes(id) ?
+    <HeartTwoTone style={{marginRight: 5}} onClick={onClickLikeBtn}/>
+    : <HeartOutlined style={{marginRight: 5}} onClick={onClickLikeBtn}/>
     }
-  };
-  const onClickRemoveList = () => {
-    dispatch(removeListRequestAction(id));
-  };
-  return (
-    <Container>
-      <Title title={title} type='list' id={id}/>
-      <PopOverPos>
-      { likes && likes.includes(id) ?
-      <HeartTwoTone style={{marginRight: 5}} onClick={onClickLikeBtn}/>
-      : <HeartOutlined style={{marginRight: 5}} onClick={onClickLikeBtn}/>
-      }
-      <Popover
-        content={(
-          <div onClick={onClickRemoveList}>
-            Delete List
-          </div>
-        )}
-        placement='right'
-        overlayInnerStyle={{cursor: 'pointer'}}
-      >
-        <EllipsisOutlined style={{transform: 'rotate(90deg)'}}/>
-      </Popover>
-      </PopOverPos>
-    </Container>
-  )
+    <Popover
+      content={(
+        <div onClick={onClickRemoveList}>
+          Delete List
+        </div>
+      )}
+      placement='right'
+      overlayInnerStyle={{cursor: 'pointer'}}
+    >
+      <EllipsisOutlined style={{transform: 'rotate(90deg)'}}/>
+    </Popover>
+    </PopOverPos>
+  </Container>
+)
 }
 export default ListTitle
-
